9b806d285ef639c7e65f184b2f6edb00047dc916
keith <12971633+keithchoii@users.noreply.github.com>
2025-09-11 02:06:44 -0700

    v1.0.1
    Updated .gitignore

--- bot.py
+++ bot.py
@@ -242,7 +242,7 @@ def build_webhook_payload(game_info):
         # different messages on win/loss
         if home_win:
             payload = {
-                'content': f"THE {home_team} WON YESTERDAY :tada: {role_ping}come get your $6 plate :fortune_cookie:\n\nUse code: **DODGERSWIN**\n\nNot sure where? Check the links below\n_ _",
+                'content': f"The {home_team} WON yesterday :tada: {role_ping}come get your $6 plate :fortune_cookie:\n\nUse code: **DODGERSWIN**\n\nNot sure where? Check the links below\n_ _",
                 'embeds': [
                     {
                         'title': embed_title,
